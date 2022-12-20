ceb4d2b31576849f5639c683ca256d07f20e0274
jc149 <canojuan99@gmail.com>
2022-12-19 21:15:41 -0600

    hello world

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// hello world
+
 public class TestScript : MonoBehaviour
 {
     // Start is called before the first frame update
